bacb7144d91ce1f90202b02858fb60e5f74b70c4
Colin Mackay <colinmackay@Colins-Mac-mini.local>
2025-12-01 21:54:30 +0000

    Make icons styleable

--- helpers/PhosphorIcons/StyleSvg.ps1
+++ helpers/PhosphorIcons/StyleSvg.ps1
@@ -13,15 +13,52 @@ foreach ($file in $svgFiles) {
 # Add a class="Stravaig" attribute to the <svg> element, if one does not already exist
 # Save the file back to disk
     [xml]$svgXml = Get-Content -Path $file.FullName
-    $svgElement = $svgXml.DocumentElement
 
+    # Add the class to the SVG element
+    $svgElement = $svgXml.DocumentElement
     if (-not $svgElement.HasAttribute("class")) {
         $svgElement.SetAttribute("class", "Stravaig")
-        $svgXml.Save($file.FullName)
-        Write-Host "Updated file: $($file.FullName)"
-    } else {
-        Write-Host "File already has class attribute: $($file.FullName)"
+        Write-Host "Added <svg ... class=`"Stravaig`"> attribute to: $($file.FullName)"
     }
+
+    # All circle elements should get the fill="currentColor" attribute
+    $circleElements = $svgXml.GetElementsByTagName("circle")
+    foreach ($circle in $circleElements) {
+        if (-not $circle.HasAttribute("fill")) {
+            $circle.SetAttribute("fill", "currentColor")
+            Write-Host "Added <circle ... fill=`"currentColor`"> in: $($file.FullName)"
+        }
+    }
+
+    # All path elements with an opacity attribute should get fill="currentColor"
+    $pathElements = $svgXml.GetElementsByTagName("path")
+    foreach ($path in $pathElements) {
+        if ($path.HasAttribute("opacity") -and -not $path.HasAttribute("fill")) {
+            $path.SetAttribute("fill", "currentColor")
+            Write-Host "Added <path ... fill=`"currentColor`"> in: $($file.FullName)"
+        }
+    }
+
+    # All rect elements with an opacity attribute should get fill="currentColor"
+    $pathElements = $svgXml.GetElementsByTagName("rect")
+    foreach ($path in $pathElements) {
+        if ($path.HasAttribute("opacity") -and -not $path.HasAttribute("fill")) {
+            $path.SetAttribute("fill", "currentColor")
+            Write-Host "Added <rect ... fill=`"currentColor`"> in: $($file.FullName)"
+        }
+    }
+
+    # All polygon elements with an opacity attribute should get fill="currentColor"
+    $pathElements = $svgXml.GetElementsByTagName("polygon")
+    foreach ($path in $pathElements) {
+        if ($path.HasAttribute("opacity") -and -not $path.HasAttribute("fill")) {
+            $path.SetAttribute("fill", "currentColor")
+            Write-Host "Added <polygon ... fill=`"currentColor`"> in: $($file.FullName)"
+        }
+    }
+
+    $svgXml.Save($file.FullName)
+    Write-Host "Updated file: $($file.FullName)"
 }
 
 Write-Host "Completed updating SVG files."
