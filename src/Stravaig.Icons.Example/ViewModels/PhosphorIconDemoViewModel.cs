202149c73c219980991f1cdd967d9726ea400794
Colin Mackay <colinmackay@Colins-Mac-mini.local>
2025-12-01 09:42:28 +0000

    Add drop list

--- src/Stravaig.Icons.Example/ViewModels/PhosphorIconDemoViewModel.cs
+++ src/Stravaig.Icons.Example/ViewModels/PhosphorIconDemoViewModel.cs
@@ -1,3 +1,4 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using Stravaig.Avalonia.Controls.Icons;
 using System;
 using System.Collections.Generic;
@@ -5,7 +6,15 @@ using System.Linq;
 
 namespace Stravaig.Icons.Example.ViewModels;
 
-public class PhosphorIconDemoViewModel
+public partial class PhosphorIconDemoViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private KeyValuePair<PhosphorIconType, string>? _selectedIconTypeName;
+
     public List<KeyValuePair<PhosphorIconName, string>> Icons { get; } = Enum.GetValues<PhosphorIconName>().Select(n => new KeyValuePair<PhosphorIconName, string>(n, n.ToString())).ToList();
+
+    //public List<string> IconTypes { get; } = Enum.GetValues<PhosphorIconType>().Select(t => t.ToString()).ToList();
+
+    public List<KeyValuePair<PhosphorIconType, string>> IconTypes { get; } = Enum.GetValues<PhosphorIconType>().Select(t => new KeyValuePair<PhosphorIconType, string>(t, t.ToString())).ToList();
+
 }
