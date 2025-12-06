1856b162592d07bbced7c8b21ba53514ca1c6dfb
Colin Mackay <colinmackay@Colins-Mac-mini.local>
2025-12-06 21:17:35 +0000

    Fix?

--- Set-Version.ps1
+++ Set-Version.ps1
@@ -27,12 +27,16 @@ if (-not (Test-Path $outputFolder))
 
 $sha = $(git rev-list --tags --max-count=1);
 $tag = $(git describe --tags $sha);
-Write-Host "Last deployment was $tag";
 if ([string]::IsNullOrEmpty($tag))
 {
     $tag = "v0.0.0-preview.0"
-    Write-Host "No previous tag found, assuming $tag";
+    Write-Host "No previous deployment tag found, assuming $tag";
 }
+else
+{
+    Write-Host "Last deployment was $tag";
+}
+
 
 $parts = $tag.Split("-preview.");
 
